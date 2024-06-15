9bf7d30e29aeb452254db7501d69af77b03a304b
GermanSchmuker <germanschmuker@gmail.com>
2024-06-15 20:33:35 -0300

    creé los arrays de series y peliculas en cada apartado y en el home los mostré

---
+++ TP_PW1/scripts/home.js
@@ -0,0 +1,303 @@
+let arrayDeSeriesYPeliculas = [
+        /*SERIES DE AUTOS--------------------------------------------*/
+
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie5.jpg',
+        "titulo":'formula 1',
+        "categoria":'autos',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie14.jpg',
+        "titulo":'un gran viaje por europa',
+        "categoria":'autos',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie15.jpg',
+        "titulo":'race bubba wallace',
+        "categoria":'autos',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie16.jpg',
+        "titulo":'west coast customs',
+        "categoria":'autos',
+        "tipo":'serie',
+    },
+    /*SERIES De CIENCIA FICCION--------------------------------------*/
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie17.jpg',
+        "titulo":'outlander',
+        "categoria":'cienciaFiccion',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie18.jpg',
+        "titulo":'flash',
+        "categoria":'cienciaFiccion',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie19.jpg',
+        "titulo":'avatar',
+        "categoria":'cienciaFiccion',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie1.jpg',
+        "titulo":'breaking bad',
+        "categoria":'cienciaFiccion',
+        "tipo":'serie',
+    },
+    /*SERIES DE COMEDIA--------------------------------------*/
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie6.jpg',
+        "titulo":'casados con hijos',
+        "categoria":'comedia',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie7.jpg',
+        "titulo":'the office',
+        "categoria":'comedia',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie8.jpg',
+        "titulo":'seinfeld',
+        "categoria":'comedia',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie9.jpg',
+        "titulo":'un show mas',
+        "categoria":'comedia',
+        "tipo":'serie',
+    },
+    /*SERIES DE TERROR--------------------------------------*/
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie10.jpg',
+        "titulo":'true blood',
+        "categoria":'terror',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie11.jpg',
+        "titulo":'parasyte',
+        "categoria":'terror',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie12.jpg',
+        "titulo":'slasher',
+        "categoria":'terror',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie13.jpg',
+        "titulo":'the walking dead',
+        "categoria":'terror',
+        "tipo":'serie',
+    },
+    /*SERIES DE SUSPENSO--------------------------------------*/
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie3.jpg',
+        "titulo":'gossip girl',
+        "categoria":'suspenso',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie2.jpg',
+        "titulo":'pablo escobar',
+        "categoria":'suspenso',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie20.jpg',
+        "titulo":'la ley y el orden',
+        "categoria":'suspenso',
+        "tipo":'serie',
+    },
+    {
+        "href":'../pages/detalleSerie.html',
+        "portada":'../assets/images/serie21.jpg',
+        "titulo":'csi',
+        "categoria":'suspenso',
+        "tipo":'serie',
+    },  
+    /*---------------------------------------------------------*/
+    /*PELICULAS DE AUTOS --------------------------------------*/
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula3.jpg',
+        "titulo":'rapido y furioso',
+        "categoria":'autos',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula2.jpg',
+        "titulo":'rapidos y furiosos reto tokio',
+        "categoria":'autos',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula1.jpg',
+        "titulo":'rapidos y furiosos sin control',
+        "categoria":'autos',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula8.jpg',
+        "titulo":'rapidos y furiosos 7',
+        "categoria":'autos',
+        "tipo":'pelicula',
+    },
+    /*PELICULAS DE CENCIA FICCION --------------------------------------*/
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula5.jpg',
+        "titulo":'top gun maverick',
+        "categoria":'ciencia ficcion',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula15.jpg',
+        "titulo":'presagio',
+        "categoria":'ciencia ficcion',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula17.jpg',
+        "titulo":'el precio del mañana',
+        "categoria":'ciencia ficcion',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula16.jpg',
+        "titulo":'replicas',
+        "categoria":'ciencia ficcion',
+        "tipo":'pelicula',
+    },
+    /*PELICULAS DE COMEDIA --------------------------------------*/
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula12.jpg',
+        "titulo":'mama se fue de viaje',
+        "categoria":'comedia',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula4.jpg',
+        "titulo":'son como niños 2',
+        "categoria":'comedia',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula13.jpg',
+        "titulo":'un espía y medio',
+        "categoria":'comedia',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula14.jpg',
+        "titulo":'son como niños',
+        "categoria":'comedia',
+        "tipo":'pelicula',
+    },
+    /*PELICULAS DE TERROR --------------------------------------*/
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula11.jpg',
+        "titulo":'la noche del demonio 3',
+        "categoria":'terror',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula7.jpg',
+        "titulo":'cadáver',
+        "categoria":'terror',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula10.jpg',
+        "titulo":'terror en el mar',
+        "categoria":'terror',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula9.jpg',
+        "titulo":'la purga por siempre',
+        "categoria":'terror',
+        "tipo":'pelicula',
+    },
+    /*PELICULAS DE SUSPENSO --------------------------------------*/
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula18.jpg',
+        "titulo":'el francotirador la ultima masacre',
+        "categoria":'suspenso',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula19.jpg',
+        "titulo":'la isla siniestra',
+        "categoria":'suspenso',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula20.jpg',
+        "titulo":'hombre en llamas',
+        "categoria":'suspenso',
+        "tipo":'pelicula',
+    },
+    {
+        "href":'../pages/detallePeli.html',
+        "portada":'../assets/images/pelicula21.jpg',
+        "titulo":'sin rastros',
+        "categoria":'suspenso',
+        "tipo":'pelicula',
+    },
+]
+
+let contenedorDeSeriesYPeliculas = document.querySelector(".grid")
+
+arrayDeSeriesYPeliculas.forEach((miObjeto)=>{
+    contenedorDeSeriesYPeliculas.innerHTML += `
+    <a href="${miObjeto.href}">
+        <img src="${miObjeto.portada}" alt=""/>
+    </a>`
+})
